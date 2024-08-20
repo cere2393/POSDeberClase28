168862f3bdab85f4d2c536cf526a5756d98639c8
cere2393 <carlos.ramirez@tecnologicoliceocristiano.edu.ec>
2024-08-20 18:30:21 -0500

    Add files via upload

---
+++ Carrito.py
@@ -0,0 +1,72 @@
+carrito=[]
+total=0.0
+
+def mostrar_menu ():
+    print ("Bienbenido al POS")
+    print ("1. Agregar producto al carrito")
+    print ("2. Ver total del carrito")
+    print ("3. Pagar")
+    print ("4. Salir")
+def agregar_producto():
+    global total 
+    
+    producto=input("Ingrese el nombre:") 
+    precio=float(input("Ingrese el precio del producto"))
+    carrito.append({"producto":producto,"precio":precio})
+    total+=precio
+    print(f"Haz agregado {producto} al carrito por {precio}")
+
+def ver_total():
+    print(f"El total de tu carrito es {total:.2f}")
+
+def eliminar_producto():
+    global total
+    
+    nombre_producto = input("Ingrese el nombre del producto que desea eliminar: ")
+    encontrado = False
+    
+    for item in carrito:
+        if item["producto"] == nombre_producto:
+            carrito.remove(item)
+            total -= item["precio"]
+            print(f"Producto {nombre_producto} eliminado del carrito.")
+            encontrado = True
+            break
+    
+    if not encontrado:
+        print(f"Producto {nombre_producto} no encontrado en el carrito.")    
+
+def pagar():
+    global total,carrito
+    if total ==0:
+        print("Tu carrito esta vacio , no hay nada que pagar")
+    else:
+        print(f"El total a pagar es {total:.2f}")  
+        pago=float(input("ingresa la cantidad  con la que vas a pagar:"))
+        if pago>=total:
+            cambio=pago-total
+            print(f"pago realizado con exito: tu cambio es :{cambio:.2f}") 
+            
+            carrito=[]
+            total=0.0
+        else:
+            print("no tienes suficiente dinero para pagar.")    
+   
+def ejecutar():
+    while True:
+        mostrar_menu()
+        opcion= input("Selecciona una opcion")
+        if opcion== "1":
+            agregar_producto()
+        elif opcion== "2":
+            ver_total()
+        elif opcion== "3":    
+            pagar()
+        elif opcion== "4":                   
+            print("Gracias por usar el post ,!hasta luego!")
+            break
+        else:
+            print("Opcion no valida, por favor intenta de nuevo")  
+
+
+ejecutar()           
